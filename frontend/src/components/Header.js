ddb1a5eb46928979578ff95b52a26d410944406a
THEMUD <themud2001@hotmail.com>
2021-02-23 00:12:57 +0200

    Did a lot of things one major thing is that I turned the Header into a portal to the body element

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,21 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div className="ui pointing menu">
-                <a className="item"><img src="/images/brand.png" /></a>
-                <a className="ui pointing dropdown link item">SHOP<i className="dropdown icon" /></a>
+const Header = () => {
+    return ReactDOM.createPortal(
+        <div className="ui fixed top pointing menu">
+            <a className="item"><img src="/images/brand.png" /></a>
+            <a className="item">SHOP<i className="dropdown icon" /></a>
+            <a className="item">ABOUT</a>
+            <a className="item">CONTACT</a>
 
-                <div className="right menu">
-                    <a className="item"><i className="cart icon" /></a>
-                    <a className="item"><i className="user icon" /></a>
-                </div>
+            <div className="right menu">
+                <a className="item"><i className="cart icon" /></a>
+                <a className="item"><i className="user icon" /></a>
             </div>
-        );
-    }
+        </div>,
+        document.querySelector("body")
+    );
 }
 
 export default Header;
