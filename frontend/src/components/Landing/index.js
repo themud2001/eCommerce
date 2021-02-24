e7ef84a5fe13f7e697ace66eab500ef57c094d75
THEMUD <themud2001@hotmail.com>
2021-02-25 01:08:39 +0200

    Developing the landing page

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import "./styles.scss";
+
 const Landing = () => {
     return (
         <div className="landing">
-
+            <div className="text-section">
+                <h1 className="title">eCommerce - Online Shop</h1>
+            </div>
         </div>
     );
 };
