e0d00cd2f377f8caa523e3552ceabeae6048f304
THEMUD <themud2001@hotmail.com>
2021-02-28 23:38:27 +0200

    Finished the Footer component

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { FaCcPaypal, FaCcMastercard, FaCcVisa } from "react-icons/fa";
 
 import "./styles.scss";
 
 const Footer = () => {
     return (
         <div className="footer">
-            hey
+            <div className="title-container">
+                <h1>eCommerce - Online Shopping</h1>
+            </div>
+
+            <div className="payment-methods">
+                <FaCcPaypal color="white" size={32} />
+                <FaCcMastercard color="white" size={32} />
+                <FaCcVisa color="white" size={32} />
+            </div>
         </div>
     );
 };
