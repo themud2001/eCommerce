3214ae3f53efc376795748f38e1efda49eb78ecc
THEMUD <themud2001@hotmail.com>
2021-03-01 00:21:52 +0200

    Added placeholder reducers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,10 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 
+import reducers from "./reducers";
 import App from "./components/App/";
 
-const store = createStore();
+const store = createStore(reducers);
 
 ReactDOM.render(
     <Provider store={store}>
