09e6fd13a0f89935b1d9eef4be2b3a7ac8e0fc4a
THEMUD <themud2001@hotmail.com>
2021-03-01 00:19:52 +0200

    Started working on the redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 
 import App from "./components/App/";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector("#root")
+);
