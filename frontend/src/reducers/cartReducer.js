51c3ae612da1b621ae70a607f41cbcc7a5d7b59e
THEMUD <themud2001@hotmail.com>
2021-03-02 21:09:47 +0200

    Fixed the cart reducer and added Redux DevTools

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 const cartReducer = (state={}, action) => {
     switch(action.type) {
         case "ADD_TO_CART":
-            return { ...state, action.payload };
+            return { ...state, [action.payload.id]: action.payload };
         case "REMOVE_FROM_CART":
             return _.omit(state, action.payload);
     }
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
 
 export default combineReducers({
-    test: 12
+    test: () => 10
 });
