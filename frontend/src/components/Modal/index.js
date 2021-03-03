ab2fca2f352791303f8c2bace356e8753576229a
THEMUD <themud2001@hotmail.com>
2021-03-04 00:50:07 +0200

    Styling and editing the Modal component

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { FaTimes } from "react-icons/fa";
 
 import "./styles.scss";
 
 const Modal = () => {
     return ReactDOM.createPortal(
         <div className="modal">
-            <div className="header">
-                <h1>Title</h1>
-            </div>
-
             <div className="content">
-                Items
-            </div>
+                <div className="header">
+                    <h1>Title</h1>
+                    <FaTimes size={26} />
+                </div>
 
-            <div className="checkout">
+                <div className="items-list">
+                    Items
+                </div>
+
+                <div className="total">
+                    <h3>Your total is: 9.99$</h3>
+                </div>
+
                 <button>CHECKOUT</button>
             </div>
         </div>,
