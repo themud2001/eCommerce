6dd42990d7eed5aca67d2f681962113427bc91f8
THEMUD <themud2001@hotmail.com>
2021-03-09 23:29:01 +0200

    Styling both the About and Contact components

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -5,35 +5,33 @@ import "./styles.scss";
 const About = () => {
     return (
         <div className="about-container">
-            <div className="huge-card">
-                <div className="header">
-                    <h1>About</h1>
+            <div className="header">
+                <h1>About</h1>
+            </div>
+
+            <div className="content">
+                <div className="first-section">
+                    <img width="300" height="400" />
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                        mollit anim id est laborum.
+                    </p>
                 </div>
 
-                <div className="content">
-                    <div className="first-section">
-                        <img width="300" height="400" />
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.
-                        </p>
-                    </div>
-
-                    <div className="second-section">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.
-                        </p>
-                        <img width="300" height="400" />
-                    </div>
+                <div className="second-section">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                        mollit anim id est laborum.
+                    </p>
+                    <img width="300" height="400" />
                 </div>
             </div>
         </div>
--- frontend/src/components/Contact/index.js
+++ frontend/src/components/Contact/index.js
@@ -1,7 +1,28 @@
 import React from "react";
 
+import "./styles.scss";
+
 const Contact = () => {
-    return <div>Test</div>;
+    return (
+        <div className="contact-container">
+            <div className="header">
+                <h1>Contact Us</h1>
+            </div>
+
+            <div className="form-container">
+                <form>
+                    <label htmlFor="name">Full Name</label>
+                    <input name="name" />
+
+                    <label htmlFor="email">E-mail</label>
+                    <input name="email" />
+
+                    <label htmlFor="message">Your message</label>
+                    <textarea name="message"></textarea>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Contact;
