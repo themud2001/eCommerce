111bc6a64ab0b32ac471269a5c7b31d8a40acbc0
THEMUD <themud2001@hotmail.com>
2021-03-11 19:03:21 +0200

    Developing the Form component

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -4,23 +4,38 @@ import { Field, reduxForm } from "redux-form";
 import "./styles.scss";
 
 class Form extends React.Component {
+    renderInput({ input, label, meta }) {
+        console.log(meta);
+        if(input.name === "message") {
+            return (
+                <React.Fragment>
+                    <label htmlFor={input.name}>{label}</label>
+                    <textarea {...input}></textarea>
+                </React.Fragment>
+            );
+        }
+
+        return (
+            <React.Fragment>
+                <label htmlFor={input.name}>{label}</label>
+                <input {...input} />
+            </React.Fragment>
+        );
+    }
+
     render() {
         return (
             <form>
-                {/* <label htmlFor="name">Full Name</label>
-                <input name="name" />
-
-                <label htmlFor="email">E-mail</label>
-                <input name="email" />
-
-                <label htmlFor="message">Your message</label>
-                <textarea name="message"></textarea>
-
-                <button type="submit">SUBMIT</button> */}
-                <Field />
+                <Field name="name" component={this.renderInput} label="Full Name" />
+                <Field name="email" component={this.renderInput} label="E-mail" />
+                <Field name="message" component={this.renderInput} label="Your Message" />
+                <button type="submit">SUBMIT</button>
             </form>
         );
     }
 }
 
-export default Form;
+export default reduxForm({
+    form: "contactForm",
+    validate: () => 10
+})(Form);
