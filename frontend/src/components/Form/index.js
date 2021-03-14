25ce5f180cc64d672efd470b84efa1264947ed09
THEMUD <themud2001@hotmail.com>
2021-03-15 00:23:58 +0200

    Removed the styling from the Form component to make it an isolated component, to make the styling dependant on the component that implements the Form component and to ahieve code reusability in general

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-import "./styles.scss";
-
 const Form = ({ children, onSubmit, ...props }) => {
     const { register, handleSubmit, errors } = useForm({ mode: "onTouched" });
 
