79133d6b55ed919079f780cd192d39082c64b03f
THEMUD <themud2001@hotmail.com>
2021-03-15 22:57:39 +0200

    Removed the redux-form import

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
 import cartReducer from "./cartReducer";
 
 export default combineReducers({
     cart: cartReducer,
-    form: formReducer
 });
