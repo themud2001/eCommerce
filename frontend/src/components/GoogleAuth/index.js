6d4f453ba4f35c7834583c34c7ba94bfe4076295
THEMUD <themud2001@hotmail.com>
2021-03-22 22:31:56 +0200

    Cleaned the old initialization for the gapi object

--- frontend/src/components/GoogleAuth/index.js
+++ frontend/src/components/GoogleAuth/index.js
@@ -5,18 +5,10 @@ import authObject from "../../GoogleAuth";
 import { changeSignedIn, updateGoogleAuthObject } from "../../actions";
 
 class GoogleAuth extends React.Component {
-    async componentDidMount() {
-        console.log(await authObject);
-        window.gapi.load("client:auth2", () => {
-            window.gapi.client.init({
-                clientId: "104324073390-am0n6j89k116lasg877tjnc2p15lceaj.apps.googleusercontent.com",
-                scope: "email"
-            }).then(() => {
-                this.auth = window.gapi.auth2.getAuthInstance();
-                this.handleAuthChange(this.auth.isSignedIn.get());
-                this.auth.isSignedIn.listen(this.handleAuthChange);
-                this.props.updateGoogleAuthObject(this.auth);
-            });
+    componentDidMount() {
+        authObject.then(object => {
+            this.handleAuthChange(object.isSignedIn.get());
+            object.isSignedIn.listen(this.handleAuthChange);
         });
     }
 
