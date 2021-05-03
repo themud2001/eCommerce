56aa0859c788e1686c83d813092667907195bc5a
THEMUD <themud2001@hotmail.com>
2021-05-03 23:27:32 +0300

    Developing routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,31 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+router.route("/")
+    .post((req, res, next) => {
+        const { username, email, password } = req.body;
+
+        User.findOne({
+            $or: [
+                { username },
+                { email }
+            ]
+        }, (error, result) => {
+            if (error) return next(error);
+
+            if(result) return res.status(400).json({ error: "User already exists" });
+        });
+    });
+
 router.route("/:id")
     .get((req, res, next) => {
         const { id } = req.params;
 
-        if(id.length !== 24) return next();
+        if (id.length !== 24) return next();
 
         User.findById(id, (error, result) => {
-            if(error) return next(error);
-            if(!result) return next();
+            if (error) return next(error);
+            if (!result) return next();
 
             res.status(200).json(result);
         });
