ae645e6bb9613c5976f71afcb3d64b646ed60614
THEMUD <themud2001@hotmail.com>
2021-05-11 15:03:19 +0300

    Removed the old code for the login and the sign up pages/paths

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -27,23 +27,6 @@ router.post("/login", async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-
-    /*User.findOne({ email })
-        .exec()
-        .then(async user => {
-            if (!user) return res.status(400).json({ error: "User does not exist" });
-
-            const isMatch = await user.matchPassword(password);
-
-            if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
-
-            const token = user.getToken();
-
-            res.status(200)
-                .cookie("token", token, { httpOnly: true })
-                .json(token);
-        })
-        .catch(next);*/
 });
 
 router.post("/signup", async (req, res, next) => {
@@ -81,31 +64,6 @@ router.post("/signup", async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-
-    /*User.findOne({
-        $or: [
-            { username: { $regex: new RegExp("^" + username + "$", "i") } },
-            { email }
-        ]
-    })
-        .exec()
-        .then(async result => {
-            if (result) return res.status(400).json({ error: "User already exists" });
-
-            try {
-                await User.validate({ username, email, password }, ["username", "email", "password"]);
-            } catch (error) {
-                return res.status(400).json({ error: error.errors });
-            }
-
-            const user = await User.create({ username, email, password });
-            const token = user.getToken();
-
-            res.status(200)
-                .cookie("token", token, { httpOnly: true })
-                .json(token);
-        })
-        .catch(next);*/
 });
 
 module.exports = router;
