6f83e362abbd0878823387741d3415b98cd5867a
THEMUD <themud2001@hotmail.com>
2021-05-11 15:02:23 +0300

    Refactored the code for the signup page to be using async/await

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -46,11 +46,43 @@ router.post("/login", async (req, res, next) => {
         .catch(next);*/
 });
 
-router.post("/signup", (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
     let { username, email, password } = req.body;
     email = email.toLowerCase();
 
-    User.findOne({
+    try {
+        let user = await User.findOne({
+            $or: [
+                { username: { $regex: new RegExp("^" + username + "$", "i") } },
+                { email }
+            ]
+        });
+
+        if (user) return res.status(400).json({ error: "User already exists" });
+
+        try {
+            await User.validate({ username, email, password }, ["username", "email", "password"]);
+        } catch (error) {
+            return res.status(400).json({ error: error.errors });
+        }
+
+        user = await User.create({ username, email, password });
+        const token = user.getToken();
+        const options = {
+            httpOnly: true,
+            expires: new Date(
+                Date.now() + process.env.JWT_COOKIE_EXPIRY * 24 * 60 * 60 * 1000
+            )
+        };
+
+        res.status(200)
+            .cookie("token", token, options)
+            .json(token);
+    } catch (error) {
+        next(error);
+    }
+
+    /*User.findOne({
         $or: [
             { username: { $regex: new RegExp("^" + username + "$", "i") } },
             { email }
@@ -73,7 +105,7 @@ router.post("/signup", (req, res, next) => {
                 .cookie("token", token, { httpOnly: true })
                 .json(token);
         })
-        .catch(next);
+        .catch(next);*/
 });
 
 module.exports = router;
