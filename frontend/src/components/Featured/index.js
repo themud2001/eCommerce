c37534a06801deff3433e60894b27a906f344d05
THEMUD <themud2001@hotmail.com>
2021-05-17 11:07:52 +0300

    Removed the old testing 'database' code

--- frontend/src/components/Featured/index.js
+++ frontend/src/components/Featured/index.js
@@ -5,13 +5,6 @@ import { getProducts } from "../../actions";
 import Card from "../Card";
 import "./styles.scss";
 
-// const products = [
-//     { id: 1, title: "T-Shirt", price: 4.99 },
-//     { id: 2, title: "Skirt", price: 14.99 },
-//     { id: 3, title: "Hoody", price: 36.99 },
-//     { id: 4 , title: "Pants", price: 9.99 }
-// ];
-
 const Featured = ({ error, products, getProducts }) => {
     useEffect(() => {
         getProducts();
